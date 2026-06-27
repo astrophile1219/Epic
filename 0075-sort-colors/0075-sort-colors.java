19ab25b642e593d888bd8f49f68ae3a5b66fa3fa
mr_uniqueee <166849007+astrophile1219@users.noreply.github.com>
2026-06-27 16:35:06 +0530

    Time: 0 ms (100%), Space: 43.6 MB (24.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,24 +3,24 @@ class Solution {
         int n = nums.length;
         int a = 0;
         int b = 0;
-        int c = 0;
+        // int c = 0;
         for (int x : nums) {
             if (x == 0) a++;
-            else if (x == 1) b++;
-            else c++;
+            if (x == 1) b++;
+            // else c++;
         }
-        int i = 0;
-        while(a != 0) {
-            nums[i++] = 0;
-            a--;
-        }
-        while(b != 0) {
-            nums[i++] = 1;
-            b--;
-        }
-        while(c != 0) {
-            nums[i++] = 2;
-            c--;
+        for (int i = 0; i<n; i++) {
+            if (a>0) {
+                nums[i] = 0;
+                a--;
+            }
+            else if (b > 0) {
+                nums[i] = 1;
+                b--;
+            }
+            else {
+                nums[i] = 2;
+            }
         }
     }
 }
